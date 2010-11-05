6369a9ba9af2028ee8eb3f914362650ae6c6a5bf
Sabart Otto <seberm@gmail.com>
2010-11-05 20:55:23 +0100

    Added comments

--- src/scheduler/scheduledialog.h
+++ src/scheduler/scheduledialog.h
@@ -22,6 +22,7 @@
 #ifndef SCHEDULEDIALOG_H
 #define SCHEDULEDIALOG_H
 
+
 #include <QDialog>
 #include <QDateTime>
 #include <QModelIndex>
@@ -31,43 +32,82 @@ namespace Ui {
     class ScheduleDialog;
 }
 
-
+//! ScheduleDialog is dialog for inserting or editing of schedules
 class ScheduleDialog : public QDialog {
-    Q_OBJECT
+    
+	Q_OBJECT
 	
 	Q_ENUMS(ScheduleActions)
+		
 			
 public:
+	
+	//! Constructor
+	/*!
+	  \param parent pointer to parent widget
+	*/
 	explicit ScheduleDialog(QWidget *parent = 0);
+	
+	//! Overloaded contructor
+	/*!
+	  This constructor is called when the schedule is edited.
+	  \param QModelIndex 
+	  \param parent pointer to parent widget
+	*/
 	ScheduleDialog(const QModelIndex &, QWidget *parent = 0);
+	
+	//! Destructor
     ~ScheduleDialog();
 	
+	//! Enum which indicates editation or insertion of schedule
 	enum ScheduleActions { Add, Edit };
 
 	
 protected:
+	
+	//! When the language is changed the window is simply retranslated
     void changeEvent(QEvent *e);
 
 	
 private:
+	
+	//! Initializes combo boxes, date and time
 	void initialize();
+	
+	//! Add or Edit the new schedule into the list and DB
 	void doSchedule();
+	
+	//! Makes the connections between objects
 	void makeConnections();
 	
+	//! The Dialog pointer
 	Ui::ScheduleDialog *ui;
+	
+	//! Enum valiable
+	/*!
+	  \sa ScheduleActions
+	*/
 	ScheduleDialog::ScheduleActions dialogAction;
+	
+	//! Represents the ID of schedule which is modified; if the schedule is inserted, the value is 0
 	int _scheduleID;
 	
 	
 signals:
+	
 	//! Is emitted when is the update of schedules needed
 	void changed();
 	
 	
 private slots:
+	
+	//! The schedule dialog is accepted
 	void scheduleAccepted();
 	
+	//! Shows the add category input dialog
 	void addCategory();
+	
+	//! Removes current category from combo box
 	void removeCategory();
 	
 };
